be980b35cf9a7be8622a02e214c2280c220dbb3e
leeebs <66204538+leeebs@users.noreply.github.com>
2021-07-08 23:57:13 +0900

    Update game_framework.py

--- dodge_game/game_framework.py
+++ dodge_game/game_framework.py
@@ -3,7 +3,7 @@ class GameState:
     def __init__(self, state):
         self.enter = state.enter
         self.exit = state.exit
-        self.pause = state.pause
+        self.pause = state.pause 
         self.resume = state.resume
         self.handle_events = state.handle_events
         self.update = state.update
